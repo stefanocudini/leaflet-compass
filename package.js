0a79960dbd463f8800d329a65578d5f6d30881a8
stefanocudini <stefano.cudini@gmail.com>
2016-06-09 11:39:40 +0200

    update package.js

--- package.js
+++ package.js
@@ -1,9 +1,12 @@
 Package.describe({
-	summary: "Leaflet Control Compass"
+	summary: "Leaflet Control Compass",
+	name: "stefcud:leaflet-compass",
+	version: "0.0.1",
+	git: "https://github.com/stefanocudini/leaflet-compass.git"
 });
 
 Package.on_use(function (api, where) {
-	api.add_files('dist/leaflet-compass.min.js', 'client');
-	api.add_files('dist/leaflet-compass.min.css', 'client');
+	api.add_files('dist/leaflet-compass.src.js', 'client');
+	api.add_files('dist/leaflet-compass.src.css', 'client');
 	api.add_files('images/compass-icon.png', 'client');
 });
